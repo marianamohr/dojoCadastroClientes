c82c1653aa84241b624f681092dbb291699c2097
FernandaPorto <fernanda.oliveira.mec@gmail.com>
2021-06-01 17:06:42 -0300

    Ajeitando a funcao das rotas fora do render do APP -> Faltou o Returngit add .hahaha...

--- dojocadastro/src/App.js
+++ dojocadastro/src/App.js
@@ -5,23 +5,41 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Clientes from './pages/ClientesCadastrados';
 import NotFound from './pages/LoginNotFound';
+import { Link } from 'react-router-dom';
 
 
 class  App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  renderHome = () =>  (<Home />)
+  
+  renderLogin = () => (<Login />)
+
+  renderClients = () => (<Clientes />)
+
+  renderRotes = () => {
+    return ( 
+    <Switch>
+        <Route exact path="/" render={({ match }) => this.renderHome(match) }/>
+        <Route path="/login" render={ ({ match }) => this.renderLogin(match) } />
+        <Route path="/clients" render={ ({ match }) => this.renderClients(match) } />
+        <Route component={ NotFound } />
+    </Switch>
+    )
+  }
 
   render() {
   return (
-    <div>
+    <section>
       <h1>Nosso Dojo</h1>
-      <Switch>
-      <Route exact path="/" component={ Home } />
-      <Route path="/login" component={ Login } />
-      <Route path="/clients" component={ Clientes } />
-      <Route component={ NotFound } />
-    </Switch>
-    </div>
-   );
- }
+      <Link to="/">Home</Link>
+      {this.renderRotes()}
+    </section>
+  )
+  }
 }
 
 export default App;
