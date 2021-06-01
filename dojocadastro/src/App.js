a0a9d8e124c10af98bf91354aaaf4346b2e1df66
FernandaPorto <fernanda.oliveira.mec@gmail.com>
2021-06-01 16:21:01 -0300

    Adiciona estrutura inicial pagina de cadastro

--- dojocadastro/src/App.js
+++ dojocadastro/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Route, Switch } from 'react-router';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Clientes from './pages/ClientesCadastrados';
+import NotFound from './pages/LoginNotFound';
 
-function App() {
+
+class  App extends React.Component {
+
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Nosso Dojo</h1>
+      <Switch>
+      <Route exact path="/" component={ Home } />
+      <Route path="/login" component={ Login } />
+      <Route path="/clients" component={ Clientes } />
+      <Route component={ NotFound } />
+    </Switch>
     </div>
-  );
+   );
+ }
 }
 
 export default App;
