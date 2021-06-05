f151015ed7b8c65c406535bc627862c2258d88ee
FernandaPorto <fernanda.oliveira.mec@gmail.com>
2021-06-04 22:28:54 -0300

    Adiciona alteracao na funcao do handleclick de ordenacao dos clientes cadastrados para adequar a condicao de retornar a ordem original

--- dojocadastro/src/pages/ClientesCadastrados.jsx
+++ dojocadastro/src/pages/ClientesCadastrados.jsx
@@ -2,33 +2,38 @@ import React from 'react';
 import { connect } from 'react-redux';
 import excluirAction from '../actions/actionDelete';
 
- class ClientesCadastrados extends React.Component {
-   constructor(props) {
-     super(props);
-     this.state = {
-       users: [],
-     }
-   }
+class ClientesCadastrados extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const { listUsers } = this.props;
+
+    this.state = {
+      btnStatus: false,
+      users: listUsers,
+    }
+  }
 
   handleClick = () => {
+    const { btnStatus } = this.state;
     const { listUsers } = this.props;
-    this.setState({
-      users: listUsers.sort((a, b) => {
-        if (a.nome < b.nome) return -1;
-        if (a.nome > b.nome) return 1;
-        return 0;
-      })
+    const users = [...this.props.listUsers].sort((a, b) => {
+      if (a.nome < b.nome) return -1;
+      if (a.nome > b.nome) return 1;
+      return 0;
     })
+    !btnStatus ? this.setState({btnStatus: true, users, }) : this.setState({btnStatus: false, users: listUsers});
   }
   render() {
     const { listUsers, excluir } = this.props;
+    const { users } = this.state
     return (
       <div>
         <h1>Clientes Cadastrados</h1>
         <button onClick={this.handleClick}>Ordem Alfabetica</button>
-        {listUsers === undefined ? (<h2>
-          Nenhum Usuário Cadastrado
-        </h2>) : listUsers.map((user) => {
+        {!listUsers 
+        ? <h2>Nenhum Usuário Cadastrado</h2> 
+        : users.map((user) => {
           return (
             <>
             <li key={user.nome}>
